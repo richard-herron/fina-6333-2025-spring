7f9795fd4b038ef4d343aac3528d11c12f9126d9
Richard Herron <richard_herron@icloud.com>
2025-01-31 17:33:02 -0500

    End of week 4

--- Notebooks/jupyterlab.bat
+++ Notebooks/jupyterlab.bat
@@ -1,1 +1,8 @@
-conda activate fina6333 && jupyter lab
+rem === Activate the fina6333 environment ===
+call "%USERPROFILE%\AppData\Local\miniconda3\condabin\conda.bat" activate fina6333
+
+rem === Launch Jupyter Lab ===
+call jupyter lab
+
+rem === Keep the window open to see messages ===
+pause
